0251a944e3f97a2f1b65674b1f2229aeb8e91397
Hasan <hasan@gmail.com>
2020-10-10 17:54:26 +0600

    add ui design

---
+++ lib/auth_page.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+
+class Authpage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _Authpage();
+  }
+}
+
+class _Authpage extends State<Authpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 270,
+              color: Colors.white,
+              child: Stack(
+                children: [
+                  Container(
+                    height: 200,
+                    decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            blurRadius: 2,
+                            spreadRadius: 3,
+                            offset: Offset(0, 0),
+                            color: Colors.blue,
+                          )
+                        ],
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(30),
+                            bottomRight: Radius.circular(30))),
+                  ),
+                  Positioned(
+                      bottom: 10,
+                      left: 140,
+                      top: 120,
+                      child: Container(
+                        decoration:
+                            BoxDecoration(shape: BoxShape.circle, boxShadow: [
+                          BoxShadow(
+                              color: Colors.blue,
+                              blurRadius: 4,
+                              spreadRadius: 5,
+                              offset: Offset(0, 0))
+                        ]),
+                        child: CircleAvatar(
+                          backgroundColor: Colors.white,
+                          radius: 70,
+                          child: ClipOval(
+                            child: Text('hasan'),
+                          ),
+                        ),
+                      )),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 80,
+            ),
+            Container(
+              height: 150,
+              margin: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 2,
+                      spreadRadius: 3,
+                      offset: Offset(0, 0),
+                      color: Colors.blue,
+                    )
+                  ],
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(50))),
+              padding: EdgeInsets.only(left: 40, right: 30, top: 20),
+              child: Column(
+                children: [
+                  TextFormField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(
+                        Icons.call,
+                        color: Colors.red,
+                      ),
+                      prefixStyle: TextStyle(color: Colors.red, fontSize: 25),
+                      focusColor: Colors.black,
+                      fillColor: Colors.red,
+                    ),
+                    onChanged: (vlaue) {},
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  RaisedButton(
+                      child: Text('SignIn'),
+                      color: Colors.red,
+                      onPressed: () {})
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
